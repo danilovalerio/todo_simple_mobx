b7226efeb828d413e845fab4373d89221a757b66
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-12-29 10:38:14 -0300

    autorun exemplo de como pode ser implementado

--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -9,6 +9,14 @@ class LoginStore = _LoginStore with _$LoginStore;
 
 abstract class _LoginStore with Store {
 
+  _LoginStore(){
+    ///Toda vez que o estado de um observable for alterado autorun executa
+    autorun((_){
+      print("email: $email");
+      print("pass: $password");
+    });
+  }
+
   @observable
   String email = "";
 
