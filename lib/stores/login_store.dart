fc7adce03f93e4fb867559e6af5c7b173b1e4e82
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-12-29 10:26:48 -0300

    definicao dos observables e actions

--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -9,6 +9,16 @@ class LoginStore = _LoginStore with _$LoginStore;
 
 abstract class _LoginStore with Store {
 
+  @observable
+  String email = "";
 
+  @observable
+  String password = "";
 
+  ///setar o email e password para receber o valor
+  @action
+  void setEmail(String value) => email = value;
+
+  @action
+  void setPassword(String value) => password = value;
 }
