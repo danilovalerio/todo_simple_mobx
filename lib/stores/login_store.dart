9e0134afca21e50b6d96a9fdd7892091e392ab96
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-12-29 14:39:37 -0300

    exibir ou ocultar password

--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:mobx/mobx.dart';
 
 part 'login_store.g.dart';
@@ -8,10 +5,9 @@ part 'login_store.g.dart';
 class LoginStore = _LoginStore with _$LoginStore;
 
 abstract class _LoginStore with Store {
-
-  _LoginStore(){
+  _LoginStore() {
     ///Toda vez que o estado de um observable for alterado autorun executa
-    autorun((_){
+    autorun((_) {
       print(isFormValid);
 
       /* //exemplo do uso
@@ -26,12 +22,18 @@ abstract class _LoginStore with Store {
   @observable
   String password = "";
 
+  @observable
+  bool obscurePassword = true;
+
   ///setar o email e password para receber o valor
   @action
   void setEmail(String value) => email = value;
 
   @action
   void setPassword(String value) => password = value;
+
+  @action
+  void toggleObscurePassword() => obscurePassword = !obscurePassword;
 
   ///Computed Serve para combinar stados de observable,
   ///sempre que usarmos computed tem que ter um get
@@ -45,10 +47,23 @@ abstract class _LoginStore with Store {
   bool get isFormValid => isEmailValid && isPasswordValid;
 
   bool isEmail(String em) {
-    String p = r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    String p =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regExp = RegExp(p);
 
     return regExp.hasMatch(em);
   }
 
+  @observable
+  bool loading = false;
+
+  ///Loading
+  @action
+  Future login() async {
+    loading = true;
+
+    await Future.delayed(Duration(seconds: 2));
+
+    loading = false;
+  }
 }
